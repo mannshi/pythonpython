6b512c54f734f34e8b824b4987bad518a9c2a331
mannshi <mannshi222@gmail.com>
2020-03-21 06:57:29 +0000

    四則演算できた?

--- asmd.py
+++ asmd.py
@@ -133,12 +133,30 @@ def tokenize(fname):
 #
 # コード生成
 #
-
 def gen( node ):
+
     if node.kind == NodeKind.ND_NUM :
         print( '\tpush {0}'.format( node.val ) )
         return
+
+    gen( node.lhs )
+    gen( node.rhs )
+
+    print('\tpop rdi')
+    print('\tpop rax')
     
+    if node.kind == NodeKind.ND_ADD :
+        print('\tadd rax, rdi')
+    elif node.kind == NodeKind.ND_SUB :
+        print('\tsub rax, rdi')
+    elif node.kind == NodeKind.ND_MUL :
+        print('\timul rax, rdi')
+    elif node.kind == NodeKind.ND_DIV :
+        print('\tcqo')
+        print('\tidiv rax, rdi')
+
+    print('\tpush rax')
+
     return
 #
 # コード生成　完了
